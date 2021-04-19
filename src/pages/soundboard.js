058db7691aac9bdcc1ba6402e7e62b777d57160b
Michael Allen Smith <digitalcolony@gmail.com>
2021-04-18 17:35:11 -0700

    sort

--- src/pages/soundboard.js
+++ src/pages/soundboard.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React,  { useState }  from "react"
 import { Helmet } from "react-helmet"
 import Meta from "./meta"
 import PrimaryLayout from "../layouts/PrimaryLayout"
@@ -6,20 +6,36 @@ import Sound from "../components/Sound"
 import JSONData from "../data/soundboard.json"
 
 
-
-function compareStrings(a,b) {
-  a = a.toLowerCase();
-  b = b.toLowerCase();
+function compareSounds(a,b) {
+  a = a.name.toLowerCase();
+  b = b.name.toLowerCase();
 
   return (a < b) ? -1 : (a > b) ? 1 : 0;
 }
 
-const sounds = JSONData.files;
-sounds.sort(function(a,b) {
-  return compareStrings(a.name, b.name);
-})
 
 export default () => {
+  const [sounds, setSounds] = useState(JSONData.files);
+  
+  sounds.sort(compareSounds);
+
+  // sort working
+  // filter NOT working 
+  // look at https://codesandbox.io/s/gracious-dawn-29qi6?file=/src/App.js
+  const [query, setQuery] = useState(''); 
+  const onChange =(event) => setQuery(event.target.value);
+
+  console.log("q", query);
+
+ sounds.filter(item => {
+    return (
+      item.name.toLowerCase().includes(query.toLowerCase()) ||
+      item.artist.toLowerCase().includes(query.toLowerCase())
+    );
+  });
+
+
+  
   return (
    
 
@@ -37,8 +53,16 @@ export default () => {
 
       <p>You can download these drops on <a href="https://archive.org/details/neil-rogers-show-soundboard">Archive</a>{" "}or{" "}<a href="https://github.com/NeilRogersRadio/sounds/tree/master/drops">GitHub</a>.</p>
      
+      <input 
+        type="text" 
+        placeholder="Enter item to be searched" 
+        value={ query } 
+        onChange={onChange} />
+  
+
         <div>
-          {sounds.map(drop => (
+          {
+          sounds.map((drop) => (
             <Sound src={drop.mp3} name={drop.name} artist={drop.artist} key={drop.mp3} />
           ))}
         </div>
