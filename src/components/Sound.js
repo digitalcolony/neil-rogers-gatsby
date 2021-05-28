e7f5bc57531f0ea7cb95120e1dc0f2a59c4236e2
Michael Allen Smith <digitalcolony@gmail.com>
2021-05-28 13:03:55 -0700

    button color

--- src/components/Sound.js
+++ src/components/Sound.js
@@ -3,8 +3,9 @@ import soundStyles from "../styles/sound.module.css"
 import nextId from "react-id-generator"
 
 function playSound(mp3) {
-  const sound = new Audio(mp3);
+  const sound = new Audio(mp3);  
   sound.play();
+
 }
 
 const Sound = props => {
@@ -14,22 +15,11 @@ const Sound = props => {
 
   return (
     <span className="track">
-      {/* <audio
-        id={audioId}
-        onPlay={() =>
-          (document.getElementById(buttonId).style.color = "yellow")
-        }
-        onEnded={() =>
-          (document.getElementById(buttonId).style.color = "white")
-        }
-      >
-        <source src={src} preload="none" type="audio/mp3" />
-      </audio> */}
-
+     
       <button
         id={buttonId}
         className={soundStyles.myButton}
-        onClick={() => playSound(src)}
+        onClick={() => playSound(src, buttonId)}
         title={artist}
       >
         {name}
