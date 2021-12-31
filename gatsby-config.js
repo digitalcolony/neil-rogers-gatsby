7c3bbb754248290bfa97a266af2408521dec2b62
Michael Allen Smith <digitalcolony@gmail.com>
2021-12-31 13:59:39 -0800

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-sitemap",
+    `gatsby-plugin-image`,
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
